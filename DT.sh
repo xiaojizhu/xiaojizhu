b2a9d499dd99b12afef2ef3e29014edca7f65fe8
xiaojizhu <141724403+xiaojizhu@users.noreply.github.com>
2023-10-03 14:56:28 +0800

    Add files via upload

--- DT.sh
+++ DT.sh
@@ -3,9 +3,11 @@ uid=`cat /data/system/packages.list | grep com.tencent.tmgp.pubgmhd | awk '{prin
 
 
 
-echo -e  '\033[36m 小叽叽开启保护 \033[0m'
-echo -e "\033[5;46;42;37m            【 连接成功嘻嘻哈哈 】                 \033[0m"
+echo -e "\033[5;46;42;37m            【 连接成功 】                 \033[0m"
 echo -e "\033[5;46;42;37m            【 开始载入 】                 \033[0m"
+
+
+
 
 
 
@@ -16,6 +18,7 @@ echo -ne '                   \033[1;37m  □□□□□□□□□□0% \r'
 sleep 0.1
 echo -ne '                   \033[1;31m  ■□□□□□□□□□10% \r'
 sleep 0.1
+
 
 
 iptables -A INPUT -p tcp --dport 17500 -j DROP
@@ -177,85 +180,54 @@ sleep 0.1
 echo -ne '                   \033[1;32m  ■■■■■■■■■□90% \r'
 sleep 0.1
 echo -ne '                   \033[1;32m  ■■■■■■■■■■100% \r'
-echo -e  '\033[31m 奔放中 \033[0m'
 
 
 
-echo "💫运行成功时刻为你保持护航"
+
+echo -e "\033[5;46;42;37m            【 演技决定一切 】                 \033[0m"
 
 while true
 do
 
 
+uid=`cat /data/system/packages.list | grep com.tencent.tmgp.pubgmhd | awk '{print $2}'`
 
 
-iptables -D OUTPUT -d 36.155.186.200 -j ACCEPT
-iptables -D OUTPUT -d 36.155.202.119 -j ACCEPT
-iptables -D OUTPUT -d 36.155.202.43 -j ACCEPT
-iptables -D OUTPUT -d 36.155.202.52 -j ACCEPT
-iptables -D OUTPUT -d 36.155.202.73 -j ACCEPT
-iptables -D OUTPUT -d 36.155.228.118 -j ACCEPT
-iptables -D OUTPUT -d 36.155.228.234 -j ACCEPT
-iptables -D OUTPUT -d 36.155.228.242 -j ACCEPT
-iptables -D OUTPUT -d 36.155.240.199 -j ACCEPT
-iptables -D OUTPUT -d 36.155.240.35 -j ACCEPT
-iptables -D OUTPUT -d 36.155.240.84 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.152 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.161 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.177 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.208 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.57 -j ACCEPT
-iptables -D OUTPUT -d 36.155.245.94 -j ACCEPT
-iptables -D OUTPUT -d 36.155.249.33 -j ACCEPT
-iptables -D OUTPUT -d 36.155.249.82 -j ACCEPT
-iptables -D OUTPUT -d 36.155.249.84 -j ACCEPT
-iptables -D OUTPUT -d 36.155.251.15 -j ACCEPT
 
+iptables -I OUTPUT -p all -m string --string cs.mbgame.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string nj.cschannel.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.anticheatexpert.com --algo bm -j ACCEPT
 echo "⚠️⚠️⚠️⚠️⚠️请保持后台运行⚠️⚠️⚠️⚠️⚠️"
 
 
 
 
-sleep 1.5
+sleep 10
 echo -ne '                   \033[1;31m  10s \r'
 
-sleep 1.5
+sleep 10
 echo -ne '                   \033[1;31m  20s \r'
 
-sleep 1.5
+sleep 10
 echo -ne '                   \033[1;31m  30s \r'
 
-sleep 2
+sleep 10
 echo -ne '                   \033[1;31m  40s \r'
 
-sleep 1.5
+sleep 10
 echo -ne '                   \033[1;31m  50s \r'
 
-sleep 1.5
+sleep 10
 echo -ne '                   \033[1;31m  60s \r'
 
 
-iptables -I OUTPUT -d 36.155.186.200 -j ACCEPT
-iptables -I OUTPUT -d 36.155.202.119 -j ACCEPT
-iptables -I OUTPUT -d 36.155.202.43 -j ACCEPT
-iptables -I OUTPUT -d 36.155.202.52 -j ACCEPT
-iptables -I OUTPUT -d 36.155.202.73 -j ACCEPT
-iptables -I OUTPUT -d 36.155.228.118 -j ACCEPT
-iptables -I OUTPUT -d 36.155.228.234 -j ACCEPT
-iptables -I OUTPUT -d 36.155.228.242 -j ACCEPT
-iptables -I OUTPUT -d 36.155.240.199 -j ACCEPT
-iptables -I OUTPUT -d 36.155.240.35 -j ACCEPT
-iptables -I OUTPUT -d 36.155.240.84 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.152 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.161 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.177 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.208 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.57 -j ACCEPT
-iptables -I OUTPUT -d 36.155.245.94 -j ACCEPT
-iptables -I OUTPUT -d 36.155.249.33 -j ACCEPT
-iptables -I OUTPUT -d 36.155.249.82 -j ACCEPT
-iptables -I OUTPUT -d 36.155.249.84 -j ACCEPT
-iptables -I OUTPUT -d 36.155.251.15 -j ACCEPT
+
+
+iptables -I OUTPUT -p all -m string --string cs.mbgame.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string nj.cschannel.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.anticheatexpert.com --algo bm -j ACCEPT
 
 sleep 0.5
 echo -ne '                   \033[1;31m  注意⚠️⚠️⚠️⚠️⚠️⚠️ \r'
