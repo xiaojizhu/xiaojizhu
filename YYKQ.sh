2535224e1dc70685ef982ccaaace80fe4cb2812a
xiaojizhu <141724403+xiaojizhu@users.noreply.github.com>
2023-10-08 18:31:03 +0800

    Delete YYKQ.sh

--- YYKQ.sh
+++
@@ -1,8 +0,0 @@
-uid=`cat /data/system/packages.list | grep com.tencent.tmgp.pubgmhd | awk '{print $2}'`
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10001 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10001 -j ACCEPT
-
-
-
-
-echo -ne '                   \033[1;31m  连接游戏语音成功 \r'
