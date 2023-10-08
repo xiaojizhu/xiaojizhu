ad1836a70940a6cd58a1ab1cb4473629ea123b08
xiaojizhu <141724403+xiaojizhu@users.noreply.github.com>
2023-10-08 18:30:54 +0800

    Delete YYGB.sh

--- YYGB.sh
+++
@@ -1,3 +0,0 @@
-uid=`cat /data/system/packages.list | grep com.tencent.tmgp.pubgmhd | awk '{print $2}'`
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10001 -j DROP
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10001 -j DROP
