39521888c31a511461ca944b2d09e99e00d40ccf
xiaojizhu <141724403+xiaojizhu@users.noreply.github.com>
2023-12-31 05:17:09 +0800

    Add files via upload

--- yz.sh
+++ yz.sh
@@ -1,8 +1,10 @@
-echo "连接成功"
-FILE=/storage/emulated/0/牛
+echo "文件名密码验证验证"
+echo "请创建文件并用密码命名，并放置以下目录"
+echo "/storage/emulated/0/"
+FILE=/storage/emulated/0/xiaojizhu;
 if [ ! -f "$FILE" ]; then
     echo "密码错误"
-    rm -rf $0
+    exit 1
     exit
 fi
 echo "可以使用"
